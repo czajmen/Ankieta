645be55ddbadc8e61ee5725a695755e58874148a
czajmen <kamil.czaja94@gmail.com>
2016-10-14 18:55:13 +0200

    add php api

--- js/app.js
+++ js/app.js
@@ -27,3 +27,34 @@ $('#name').keydown(function (e) {
 });
 
 
+$('#ajax').submit(function() {
+
+
+    var that = $(this),
+        url = that.attr('action'),
+        method= that.attr('method'),
+        data={};
+
+
+    that.find('[name]').each(function(index,value){
+        var that = $(this),
+            name=that.attr('name'),
+            value=that.val();
+
+        data[name]= value;
+    });
+
+    $.ajax({
+        url: url,
+        type: method,
+        data: data,
+        success: function(response){
+            console.log(response);
+        }
+    });
+
+    return false;
+
+});
+
+
